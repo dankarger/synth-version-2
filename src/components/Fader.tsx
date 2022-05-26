605b5a765cdc6156f63f515234fb946109e7dce3
dankarger <karger.dan@gmail.com>
2022-05-26 18:40:11 +0300

    adding input to Fader + ading contex dir + store.js

--- src/components/Fader.tsx
+++ src/components/Fader.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {SyntheticEvent, useState} from "react";
 import './Fader.css'
 
 
@@ -8,10 +8,22 @@ interface Props {
 
 }
 const Fader:React.FC<Props> =({name, type}:Props)=> {
+
+    const [frequency, setFrequency] = useState<number>(200)
+    const handleChange=(e:  React.ChangeEvent<HTMLInputElement>)=>{
+        console.log(e.target.value)
+    }
+
     return (
         <div className={'fader'}>
             <h2>Fader- {name}</h2>
             <h3>Type: {type}</h3>
+            <input type="range"
+                    value={frequency}
+                   onChange={handleChange}
+                   className="frequency"
+                   max="10000"
+            />
         </div>
     )
 }
