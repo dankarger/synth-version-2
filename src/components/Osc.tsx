f94f73e1b355dc0e4651a8a3f85975fc615c1632
dankarger <karger.dan@gmail.com>
2022-05-29 14:34:46 +0300

    problems with typescrypt and useContext2

--- src/components/Osc.tsx
+++ src/components/Osc.tsx
@@ -1,47 +1,79 @@
-import React, {useContext} from "react";
+import React, {useContext, useState} from "react";
 import './Osc.css'
-import {CTX} from "../context/Store"
+// import {CTX} from "../context/Store"
 
 
 interface Props {
     name: string,
     typeName: string
 }
+//
+// interface UseContextType {
+//     appState:string
+//     useState:
+// }
 
-interface UseContextType {
-    appState:string
-    useState:
-}
+
+let actx = new AudioContext();
+let out = actx.destination;
+
+let osc1 = actx.createOscillator()
+let gain1 = actx.createGain();
+let filter = actx.createBiquadFilter();
+
+
+// osc1.connect(gain1);
+gain1.connect(filter);
+filter.connect(out);
+// osc1.start();
 
 
 const Osc:React.FC<Props> =({name, typeName}:Props)=> {
-    const [appState, updateState] = useContext(CTX);
-    let {type , frequency, detune}= appState.osc1Settings;
+    // const [appState, updateState] = useContext(CTX);
+    // let {type , frequency, detune}= appState.osc1Settings;
+    const [frequency, setFrequency] = useState(200);
+    const [detune, setDetune] = useState(0);
+
+    const [ type, changeTypeState] = useState('sine')
 
 
     const change =(e: React.ChangeEvent<HTMLInputElement>)=> {
-        let {id, value} = e.target;
-        updateState({type: "CHANGE_OSC1", payload: {id, value}})
+        let {id, value} = e.target as any;
+       setFrequency(value)
+
+        // updateState({type: "CHANGE_OSC1", payload: {id, value}})
     }
     const changeType = (e: React.MouseEvent<HTMLButtonElement>)=>{
         let {id} = e.target  as HTMLInputElement;
-        updateState({type: "CHANGE_OSC1_TYPE", payload: {id}})
+        // updateState({type: "CHANGE_OSC1_TYPE", payload: {id}})
     }
 
 
     return (
         <div className={'osc-div'}>
             <h2>Osc- {name}</h2>
-            <h3>Type: {type}</h3>
+            <h3>Type: {typeName}</h3>
             <div >
-                <button onClick={()=>updateState({type:"START_OSC"})}>Start</button>
-                <button onClick={()=>updateState({type:"STOP_OSC"})}>Stop</button>
+                <button onClick={
+                    // ()=>updateState({type:"START_OSC"})
+                    ()=>{osc1.start()}
+                }>Start</button>
+                <button onClick={
+                    // ()=>updateState({type:"STOP_OSC"})
+                    ()=>{osc1.stop()}
+                }>Stop</button>
             </div>
             <div className="control">
                 <h2>OSC1</h2>
                 <div >
-                    <button onClick={()=>updateState({type:"START_OSC"})}>Start</button>
-                    <button onClick={()=>updateState({type:"STOP_OSC"})}>Stop</button>
+                    <button onClick={
+                        ()=>{osc1.start()}
+                        // ()=>updateState({type:"START_OSC"})
+                    }>Start</button>
+                    <button onClick={
+                        // ()=>updateState({type:"STOP_OSC"})
+                        ()=>{osc1.stop()}
+                    }>Stop</button>
                 </div>
                 <div className="params">
 
